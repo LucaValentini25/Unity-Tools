65b7bc5ac54b85f46e9596b437b2fbbc556e6f9c
Luca Valentini <139935542+LucaValentini25@users.noreply.github.com>
2025-03-24 17:53:54 -0300

    Update ProjectZipper.cs

--- Tools/ProjectZipper/Editor/ProjectZipper.cs
+++ Tools/ProjectZipper/Editor/ProjectZipper.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEngine;
 using CompressionLevel = System.IO.Compression.CompressionLevel;
 
-namespace com.ClasterTools..ProjectZipper.Editor
+namespace com.ClasterTools.ProjectZipper.Editor
 {
     public static class ProjectZipper
     {
